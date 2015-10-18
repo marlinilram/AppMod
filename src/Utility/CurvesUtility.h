ccb703390399532393cf840505ec50e6afddca59
xiaojhl <1511189203@qq.com>
2015-10-18 19:31:42 +0800

    Added CurvesUtility

---
+++ src/Utility/CurvesUtility.h
@@ -0,0 +1,23 @@
+#ifndef CurvesUtility_H
+#define CurvesUtility_H
+
+#include <cv.h>
+#include "BasicDataType.h"
+#include "BasicHeader.h"
+
+
+namespace CurvesUtility
+{
+  bool isBoundary(cv::Mat& primitive_img, int x, int y);
+
+  void getBoundaryImg(cv::Mat& outputImg, cv::Mat& primitive_id_img);
+
+  double distInScalarMap(STLVectorf& scalar_map, int dim, double2& src_pt, double2& tar_pt, double threshold);
+
+  bool closestPtInCurves(double2& tar_pt, std::vector<std::vector<double2> >& src_curves,
+    int& src_i, int& src_j, double& dis, 
+    STLVectorf& scalar_map = STLVectorf(0), int dim = 0, double threshold = 0.0);
+}
+
+
+#endif // !CurvesUtility_H
